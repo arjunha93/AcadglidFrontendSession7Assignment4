ed8588414a82ea568e65d77ecfdaf73383b8b330
arjunha93 <48612246+arjunha93@users.noreply.github.com>
2019-03-17 23:38:14 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var name = "Arjun H A";
+var age = 26;
+var dateofbirth = "03/05/1993";
+var placeofbirth = "mandya";
+console.log("name ",name);
+console.log("age ",age);
+console.log("dateofbirth ",dateofbirth);
+console.log("placeofbirth ",placeofbirth);
