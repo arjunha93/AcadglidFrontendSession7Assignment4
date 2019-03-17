6ee69588d89ab80a70f69b31179d9f4a4e2d216f
arjunha93 <48612246+arjunha93@users.noreply.github.com>
2019-03-18 00:13:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,63 @@
-var employee = {
-    name: 'Arjun H A',
-    age: 26,
-    salary: "60000",
-    address:  {
-      'city':'mandya'
-      'state':'karnataka',
-      'pincode':'571450'
-    }
-  
-};
-console.log("name ",employee.name);
-console.log("age ",employee.age);
-console.log("dateofbirth ",employee.salary);
-console.log("placeofbirth ",employee.address.city);
-console.log("placeofbirth ",employee.address.state);
-console.log("placeofbirth ",employee.address.pincode);
+var employee = [
+            {
+            name: 'Arjun H A',
+            age: 26,
+            salary: "60000",
+            address:  {
+              'city':'mandya'
+              'state':'karnataka',
+              'pincode':'571450'
+            }
+
+        },
+         {
+                name: 'manjunath',
+                age: 26,
+                salary: "60000",
+                address:  {
+                  'city':'mandya'
+                  'state':'karnataka',
+                  'pincode':'571450'
+                }
+
+          },
+         {
+                name: 'pradeep',
+                age: 26,
+                salary: "70000",
+                address:  {
+                  'city':'mandya'
+                  'state':'karnataka',
+                  'pincode':'571450'
+                }
+
+            },
+        {
+            name: 'raghu,
+            age: 26,
+            salary: "80000",
+            address:  {
+              'city':'mandya'
+              'state':'karnataka',
+              'pincode':'571450'
+            }
+
+        },
+        {
+            name: 'harsha',
+            age: 26,
+            salary: "90000",
+            address:  {
+              'city':'mandya'
+              'state':'karnataka',
+              'pincode':'571450'
+            }
+
+        }
+    ]
+console.log("name ",employee[0].name);
+console.log("age ",employee[0].age);
+console.log("dateofbirth ",employee[0].salary);
+console.log("placeofbirth ",employee[0].address.city);
+console.log("placeofbirth ",employee[0].address.state);
+console.log("placeofbirth ",employee[0].address.pincode);
