4ce9d8abe46f6a3332d253f997dae2928a02c905
arjunha93 <48612246+arjunha93@users.noreply.github.com>
2019-03-18 13:56:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,8 @@
-var employee = [
-            {
-            name: 'Arjun H A',
-            age: 26,
-            salary: "60000",
-            address:  {
-              'city':'mandya'
-              'state':'karnataka',
-              'pincode':'571450'
-            }
+var radius = 10;
 
-        },
-         {
-                name: 'manjunath',
-                age: 26,
-                salary: "60000",
-                address:  {
-                  'city':'mandya'
-                  'state':'karnataka',
-                  'pincode':'571450'
-                }
+function calculateArea(radius){
+   var area = 3.14  (radius  radius);
+  console.log('area : ' + area);
+}
 
-          },
-         {
-                name: 'pradeep',
-                age: 26,
-                salary: "70000",
-                address:  {
-                  'city':'mandya'
-                  'state':'karnataka',
-                  'pincode':'571450'
-                }
-
-            },
-        {
-            name: 'raghu',
-            age: 26,
-            salary: "80000",
-            address:  {
-              'city':'mandya'
-              'state':'karnataka',
-              'pincode':'571450'
-            }
-
-        },
-        {
-            name: 'harsha',
-            age: 26,
-            salary: "90000",
-            address:  {
-              'city':'mandya'
-              'state':'karnataka',
-              'pincode':'571450'
-            }
-
-        }
-    ]
-console.log("name ",employee[0].name);
-console.log("age ",employee[0].age);
-console.log("dateofbirth ",employee[0].salary);
-console.log("placeofbirth ",employee[0].address.city);
-console.log("placeofbirth ",employee[0].address.state);
-console.log("placeofbirth ",employee[0].address.pincode);
+calculateArea(radius);
