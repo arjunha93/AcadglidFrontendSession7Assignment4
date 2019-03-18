7ea36388b17afc6ca23ad5ea0f367a7615672006
arjunha93 <48612246+arjunha93@users.noreply.github.com>
2019-03-18 15:05:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var radius = 10;
+var arr = [12,34,4,5,6,7,8,9,44];
+var sum = 0;
+function getSum(arr){
+    for(var i = 0; i<arr.length;i++){
+        sum = sum + arr[i];
+    }
+    console.log("Sum : " + sum);
+}  
 
-function calculateArea(radius){
-   var area = 3.14  (radius  radius);
-  console.log('area : ' + area);
-}
-
-calculateArea(radius);
+getSum(arr);
